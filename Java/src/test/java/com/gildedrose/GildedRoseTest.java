054dad186b9c06ff9760905f6d1e7c6f3bc708b1
maryna-13 <marina35963@gmail.com>
2026-05-20 00:53:28 +0300

    Add regular item characterization test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,1 +1,23 @@
+package com.gildedrose;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void regularItemDecreasesSellInAndQuality() {
+
+        Item[] items = new Item[] {
+            new Item("foo", 10, 20)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
+}
