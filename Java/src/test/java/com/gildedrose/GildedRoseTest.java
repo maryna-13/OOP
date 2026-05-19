3e86752006f3df06f3a030b3b10918a3c72993c6
maryna-13 <marina35963@gmail.com>
2026-05-20 01:09:15 +0300

    Add test for Sulfuras behavior

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
+    @Test  //звичайний товар: sellIn і quality зменшуються на 1 після одного дня
     void regularItemDecreasesSellInAndQuality() {
 
         Item[] items = new Item[] {
@@ -21,7 +21,7 @@ class GildedRoseTest {
         assertEquals(19, app.items[0].quality);
     }
 
-    @Test
+    @Test // Aged Brie: з часом quality збільшується, а sellIn зменшується
     void agedBrieIncreasesQuality() {
         Item[] items = new Item[] {
             new Item("Aged Brie", 2, 0)
@@ -32,6 +32,22 @@ class GildedRoseTest {
 
         assertEquals(1, items[0].quality);
         assertEquals(1, items[0].sellIn);
+    }
+
+
+    @Test // Sulfuras: легендарний товар, sellIn і quality не змінюються
+    void sulfurasNeverChanges() {
+
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
     }
 
 }
