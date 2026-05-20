630c1ad56ce1d375aab443454bc29631ec34a3da
maryna-13 <marina35963@gmail.com>
2026-05-20 22:52:56 +0300

    Add files via upload

--- NumberOperations.java
+++ NumberOperations.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberOperations {
+
     public static void main(String[] args) {
+
         // числа різних типів
         int intNum1 = 10;
         int intNum2 = 30;
@@ -16,9 +18,9 @@ public class NumberOperations {
         double doubleNum4 = 80.1;
         double doubleNum5 = 100.9;
 
-
         // додавання до списку
         List<Number> allMyNumbers = new ArrayList<>();
+
         allMyNumbers.add(intNum1);
         allMyNumbers.add(doubleNum1);
         allMyNumbers.add(intNum2);
@@ -30,60 +32,98 @@ public class NumberOperations {
         allMyNumbers.add(intNum5);
         allMyNumbers.add(doubleNum5);
 
-        // виводимо числа на екран
-        System.out.println("1. Всі числа: " + allMyNumbers);
+        // виклик методів
+        printAllNumbers(allMyNumbers);
+        printIntegers(allMyNumbers);
+        printDoubleFormat(allMyNumbers);
+        separateNumbers(allMyNumbers);
+        calculateSum(allMyNumbers);
+        calculateProduct(allMyNumbers);
+        multiplyByTwo(allMyNumbers);
+    }
 
-        // виводимо числа у форматі цілих чисел
+    // виведення всіх чисел
+    public static void printAllNumbers(List<Number> numbers) {
+        System.out.println("1. Всі числа: " + numbers);
+    }
+
+    // виведення у форматі цілих чисел
+    public static void printIntegers(List<Number> numbers) {
+
         System.out.print("2. Цілі числа: ");
-        for (Number number : allMyNumbers) {
+
+        for (Number number : numbers) {
             System.out.print(number.intValue() + " ");
         }
+
         System.out.println();
+    }
 
+    // виведення у форматі дробних чисел
+    public static void printDoubleFormat(List<Number> numbers) {
 
-        // виводимо числа у форматі дробних чисел з 2ма знаками після коми
         System.out.print("3. Формат дробних чисел (2 знаки): ");
-        for (Number number : allMyNumbers) {
+
+        for (Number number : numbers) {
             System.out.print(String.format("%.2f", number.doubleValue()) + " ");
         }
-        System.out.println();
 
-        // збереження чисел в різних списках в залежності від їх типу 
+        System.out.println();
+    }
+
+    // розділення чисел за типами
+    public static void separateNumbers(List<Number> numbers) {
+
         List<Integer> intList = new ArrayList<>();
         List<Double> doubleList = new ArrayList<>();
-        for (Number number : allMyNumbers) {
+
+        for (Number number : numbers) {
+
             if (number instanceof Integer) {
                 intList.add((Integer) number);
+
             } else if (number instanceof Double) {
                 doubleList.add((Double) number);
             }
         }
+
         System.out.println("4. Цілі окремо: " + intList);
         System.out.println("5. Дробні окремо: " + doubleList);
+    }
 
+    // сума всіх чисел
+    public static void calculateSum(List<Number> numbers) {
 
-
-
-        // сума всіх чисел
         double sum = 0;
-        for (Number number : allMyNumbers) {
+
+        for (Number number : numbers) {
             sum += number.doubleValue();
         }
+
         System.out.println("6. Сума всіх чисел: " + sum);
+    }
 
-        // добуток перших п'яти чисел
+    // добуток перших п'яти чисел
+    public static void calculateProduct(List<Number> numbers) {
+
         double product = 1;
-        for (int i = 0; i < 5; i++) {
-            product *= allMyNumbers.get(i).doubleValue();
-        }
-        System.out.println("7. Добуток перших п’яти чисел: " + product);
 
-        // новий список, де кожне число початкового списку помножене на 2
+        for (int i = 0; i < 5; i++) {
+            product *= numbers.get(i).doubleValue();
+        }
+
+        System.out.println("7. Добуток перших п’яти чисел: " + product);
+    }
+
+    // список чисел, помножених на 2
+    public static void multiplyByTwo(List<Number> numbers) {
+
         List<Double> multipliedByTwo = new ArrayList<>();
-        for (Number number : allMyNumbers) {
+
+        for (Number number : numbers) {
             multipliedByTwo.add(number.doubleValue() * 2);
         }
+
         System.out.println("8. Помножені на 2: " + multipliedByTwo);
     }
 }
-
